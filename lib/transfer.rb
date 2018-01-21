8aef9d316e78f5f60f58feafd273bad2097bd057
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-21 15:01:55 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,7 +12,7 @@ class Transfer
   end
   
   def valid?
-    sender.valid? && receiver.valid? && sender.balance >= amount
+    sender.valid? && receiver.valid? && sender.balance > amount
   end
   
   def execute_transaction
